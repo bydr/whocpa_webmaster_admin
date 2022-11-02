f5f5074993d878ced87aaa4e75ff45e5a11152c5
daniil <zeveres88@gmail.com>
2022-11-02 13:04:53 +0300

    страница Настройки страница финансы Пополнение баланса добавлен вывод комментария в выплаты добавлена дополнительная фильтрация для графика (линейного) правки на страницах месенджера

---
+++ app/views/components/forms/financeRefillFilterForm.php
@@ -0,0 +1,54 @@
+<form class="form form_view-row" action="#">
+  <div class="form-row">
+    <div class="form-col">
+      <div class="form-control">
+        <select name="publisher" id="publisher" class="js-choice custom-select">
+          <option value="" disabled selected>ID Паблишера</option>
+          <option value="vertical1">Паблишер 1</option>
+          <option value="vertical2">Паблишер 2</option>
+          <option value="vertical3">Паблишер 3</option>
+          <option value="vertical4">Паблишер 4</option>
+        </select>
+      </div>
+    </div>
+    <div class="form-col">
+      <div class="form-control">
+        <select name="traffic" id="traffic" class="js-choice custom-select">
+          <option value="" disabled selected>Тип баланса</option>
+          <option value="traffic1">Трафик 1</option>
+          <option value="traffic2">Трафик 2</option>
+          <option value="traffic3">Трафик 3</option>
+          <option value="traffic4">Трафик 4</option>
+        </select>
+      </div>
+    </div>
+    <div class="form-col flex-initial">
+      <div class="form-control">
+        <select name="traffic" id="traffic" class="js-choice custom-select">
+          <option value="" disabled selected>Валюта</option>
+          <option value="traffic1">Валюта 1</option>
+          <option value="traffic2">Валюта 2</option>
+          <option value="traffic3">Валюта 3</option>
+          <option value="traffic4">Валюта 4</option>
+        </select>
+      </div>
+    </div>
+    <div class="form-col flex-initial">
+      <div class="form-control">
+        <input type="text" placeholder="Сумма">
+      </div>
+    </div>
+  </div>
+  <div class="form-row">
+    <div class="form-col">
+      <div class="form-control">
+        <textarea class="textarea-element" name="comment" placeholder="Комментарий"></textarea>
+      </div>
+    </div>
+    <div class="form-col flex-initial">
+      <div class="button_group button_group-row w-100 justify-center">
+        <button class="button button_accent">Пополнить</button>
+      </div>
+    </div>
+  </div>
+</form>
